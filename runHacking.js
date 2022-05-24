7597c76f84929b4322beb50506891af8977ed02a
Collin O'Connor <collin.d.oconnor@gmail.com>
2022-05-24 03:42:34 -0500

    Adding new items.

---
+++ runHacking.js
@@ -0,0 +1,66 @@
+function localeHHMMSS(ms = 0) {
+  if (!ms) {
+    ms = new Date().getTime()
+  }
+
+  return new Date(ms).toLocaleTimeString()
+}
+
+export async function main(ns) {
+  ns.tprint(`[${localeHHMMSS()}] Starting runHacking.js`)
+
+  let hostname = ns.getHostname()
+
+  if (hostname !== 'home') {
+    throw new Exception('Run the script from home')
+  }
+
+  const homeRam = ns.getServerRam('home').shift()
+
+  if (homeRam >= 32) {
+    ns.tprint(`[${localeHHMMSS()}] Spawning spider.js`)
+    await ns.run('spider.js', 1, 'purchaseServers.js')
+
+	ns.tprint(`[${localeHHMMSS()}] Spawning hacknetManager.js`)
+    await ns.run('spider.js', 1, 'hacknetManager.js')
+
+	if(homeRam >= 256) {
+		if (player.inteligence > 0) { 
+			ns.tprint(`[${localeHHMMSS()}] Spawning createPrograms.js`)
+			await ns.run('spider.js', 1, 'createPrograms.js')
+		} else {
+			ns.tprint(`[${localeHHMMSS()}] Spawning buyPrograms.js`)
+			await ns.run('spider.js', 1, 'buyPrograms.js')
+		}
+	}
+
+    let player = ns.getPlayer();
+    if (player.hasTixApiAccess) {
+      ns.tprint(`[${localeHHMMSS()}] Player has Tix API Access`)
+      try {
+        ns.stock.short();
+      }
+      catch (e) {
+        ns.tprint(e.toString())
+        const cantShort = e.toString().includes("BitNode-8")
+        if (player.has4SDataTixApi) {
+          if (cantShort) {
+            ns.tprint(`[${localeHHMMSS()}] Spawning startStock.js`)
+            await ns.run('startStock.js', 1)
+          } else {
+            ns.tprint(`[${localeHHMMSS()}] Spawning stockMarketer4s.js`)
+            await ns.run('stockMarketer4s.js', 1)
+          }
+        } else if (!player.has4SDataTixApi && !cantShort) {
+          ns.tprint(`[${localeHHMMSS()}] Spawning stockMarketer.js`)
+          await ns.run('stockMarketer.js', 1)
+        }
+      }
+    }
+    ns.tprint(`[${localeHHMMSS()}] Spawning spider.js to mainHack.js`)
+    ns.spawn('spider.js', 1, 'mainHack.js')
+  } else {
+    ns.tprint(`[${localeHHMMSS()}] Spawning spider.js to mainHack.js`)
+    ns.spawn('spider.js', 1, 'mainHack.js')
+  }
+}
--- spider.js
+++ spider.js
@@ -34,7 +34,7 @@ function localeHHMMSS(ms = 0) {
 }
 
 export async function main(ns) {
-  ns.tprint(`[${localeHHMMSS()}] Starting spider.ns`)
+  ns.tprint(`[${localeHHMMSS()}] Starting spider.js`)
 
   const scriptToRunAfter = ns.args[0]
 
@@ -145,8 +145,8 @@ export async function main(ns) {
   setItem(settings().keys.serverMap, serverMap)
 
   if (!scriptToRunAfter) {
-    ns.tprint(`[${localeHHMMSS()}] Spawning mainHack.ns`)
-    ns.spawn('mainHack.ns', 1)
+    ns.tprint(`[${localeHHMMSS()}] Spawning mainHack.js`)
+    ns.spawn('mainHack.js', 1)
   } else {
     ns.tprint(`[${localeHHMMSS()}] Spawning ${scriptToRunAfter}`)
     ns.spawn(scriptToRunAfter, 1)
